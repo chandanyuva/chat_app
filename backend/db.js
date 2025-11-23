f5282fc2f64cdc706c9eadf054ea1eae528ca28b
chandanyuva <chandanyuva1511@gmail.com>
2025-11-23 18:22:31 +0530

    add a dev compose file

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://mongo:27017/chatapp"); // only works inside docker network
-    // await mongoose.connect("mongodb://localhost:27017/chatapp"); // for local (outside docker) dev
+    // await mongoose.connect("mongodb://mongo:27017/chatapp"); // only works inside docker network
+    await mongoose.connect("mongodb://localhost:27017/chatapp"); // for local (outside docker) dev
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("MongoDB connection error:", err);
